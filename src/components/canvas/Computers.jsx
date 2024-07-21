8e73d6eed3268c6e25a46beeb803d4d4ca936974
Narmathaa JB <balanarmathaa@gmail.com>
2024-07-21 15:32:30 +0530

    3d

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,6 @@
-import {Suspense, useEffect, useState} from 'react';
-import {Canvas} from '@react-three/fiber';
-import {OrbitControls, Preload, useGLTF} from '@react-three/drei';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
 
 import CanvasLoader from '../Loader';
 
@@ -10,19 +10,8 @@ const Computers = () => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black"/>
       <pointLight intensity={1}/>
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   )
