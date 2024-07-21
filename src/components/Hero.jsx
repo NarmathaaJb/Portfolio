1612186d9f5c60fa0dbc1a0b62014ada220da6c4
Narmathaa JB <balanarmathaa@gmail.com>
2024-07-21 15:21:39 +0530

    3d

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,10 +14,11 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Narmathaa</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop user interfaces, user experiences and web applications</p>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop user interfaces, user <br className='sm:block hidden'/> experiences and web applications</p>
         </div>
-
       </div>
+
+      <ComputersCanvas/>
     </section>
   )
 }
