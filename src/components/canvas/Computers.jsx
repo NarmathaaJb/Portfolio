0e178c79044b01af9902aed8a524677f2d62bb45
Narmathaa JB <balanarmathaa@gmail.com>
2024-07-21 15:38:07 +0530

    3d

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,10 @@
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, extend } from '@react-three/fiber';
 import { Suspense } from 'react';
-
+import * as THREE from 'three';
 import CanvasLoader from '../Loader';
+
+extend(THREE);
 
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
