965e2bfcfe7157c54a385a1b25b23ec251f1fa52
Narmathaa JB <balanarmathaa@gmail.com>
2024-07-25 18:23:12 +0530

    updated

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,7 +2,6 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import { Canvas, extend } from '@react-three/fiber';
 import { Suspense } from 'react';
 import * as THREE from 'three';
-import CanvasLoader from '../Loader';
 
 extend(THREE);
 
@@ -10,10 +9,19 @@ const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black"/>
+      <hemisphereLight
+        skyColor={'white'}
+        groundColor={'#444444'}
+        intensity={0.3}
+      />
       <pointLight intensity={1} />
       <spotLight
       position={[-20,50,10]}
+      angle={0.12}
+      penumbra={1}
+      intensity={1}
+      castShadow
+      shadow-mapSize={1024}
       />
       <primitive
         object={computer.scene}
